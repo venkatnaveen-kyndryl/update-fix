a43e3683a688bd0fbb63b17f1d84a4738fc2adce
venkatnaveen-kyndryl <104070618+venkatnaveen-kyndryl@users.noreply.github.com>
2022-04-20 17:04:10 +0530

    Update update_fix.sh

--- update_fix.sh
+++ update_fix.sh
@@ -49,7 +49,7 @@ rpm -rebuilddb
 
 wget -P /tmp https://github.com/venkatnaveen-kyndryl/update-fix/raw/main/ibm-router-agent-8.4-5.el8.noarch.rpm
 wget -P /tmp https://github.com/venkatnaveen-kyndryl/update-fix/raw/main/ibm-router-agent-8.4-5.el8.noarch.rpm
-rpm -ivh --replacefiles --replacepkgs /tmp/ibm-router-agent-8.4-5.el8.noarch.rpm ibm-router-agent-8.4-5.el8.noarch.rpm
+rpm -ivh --replacefiles --replacepkgs /tmp/ibm-router-agent-8.4-5.el8.noarch.rpm /tmp/ibm-router-agent-8.4-5.el8.noarch.rpm
 
 # running updates
 dnf update -y
